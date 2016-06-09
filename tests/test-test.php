fe504249c68c3181016d0d4ab58adb832190d7ac
Brandon Braner <brandon.braner@ave81.com>
2016-06-09 16:56:02 -0500

    and again

--- tests/test-test.php
+++ tests/test-test.php
@@ -4,7 +4,7 @@ class TestTest extends PHPUnit_Framework_TestCase
 {
     function test_tester()
     {
-        $env = getenv();
+        $env = getenv('lpusername');
         print_r($env);
     }
 }
